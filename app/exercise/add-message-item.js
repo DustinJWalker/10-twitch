a4bb02c1330931441f8986548a6cfbd206bf9c96
Dustin Walker <dwtimp@gmail.com>
2016-10-17 14:22:33 -0500

    finished sum test

--- app/exercise/add-message-item.js
+++ app/exercise/add-message-item.js
@@ -1,1 +1,7 @@
-export default null;
+import createMessageItem from '../../app/exercise/create-message-item';
+
+export default function (list, data) {
+  const li = createMessageItem(data);
+
+  list.appendChild(li);
+}
--- app/exercise/sum-from-list.js
+++ app/exercise/sum-from-list.js
@@ -1,1 +1,8 @@
-export default null;
+export default function (li) {
+  let sum = 0;
+  for (let i = 0; i < li.children.length; i++) {
+    sum = sum + parseInt(li.children[i].innerText);
+  }
+
+  return sum;
+}
