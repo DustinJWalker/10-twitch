4770e2757273eb32fd447c16463329645a99a58e
Dustin Walker <dwtimp@gmail.com>
2016-10-19 11:08:59 -0500

    still stuck

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,9 +1,13 @@
-export default function (game) {
-  const createGameItem = document.createElement('div');
-  createGameItem.innerHTML =
-  `<div class="frame">
+export default function createGameElement(data) {
+  const game = document.createElement('div');
+  game.classList.add('game-item');
+  game.innerHTML =
+  `<div className="frame">
   <h2 class="game-item__name"</h2>
   <h3 class="game-item__popularity</h3>
   <h4 class="_id"</h4>
-  </div>`;
+  </div>
+  <img class="game-item__pic" src ="" alt=""></img>`;
+  const title = game.querySelector('.game-item__name');
+  title.innerText = game.name;
 }
