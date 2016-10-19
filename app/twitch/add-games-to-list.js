2d655dc292f7a957c26d4f75c19341cc3f3e7cfa
Dustin Walker <dwtimp@gmail.com>
2016-10-19 13:48:47 -0500

    stuck on final test

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -1,3 +1,7 @@
 import createGameElemnt from './create-game-element';
 
-export default null;
+export default function showGamesInList(parentEl, data) {
+  const li = createGameElemnt(data);
+
+  parentEl.appendChild(li);
+}
