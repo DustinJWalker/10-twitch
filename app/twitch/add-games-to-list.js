60d8cbc8c392e7699a84573e74786bf513d72cfb
Dustin Walker <dwtimp@gmail.com>
2016-10-19 13:55:29 -0500

    some progress

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -1,7 +1,12 @@
 import createGameElemnt from './create-game-element';
 
 export default function showGamesInList(parentEl, data) {
-  const li = createGameElemnt(data);
+  for (let i = 0; i < data.length; i++) {
+    array[i];
+  }
 
-  parentEl.appendChild(li);
+  const gameItem = data[i];
+
+  const el = createGameElemnt(gameItem);
+  parentEl.appendChild(el);
 }
