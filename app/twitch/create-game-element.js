db98ea589072ad98c898cfda9aef5b00fe002449
Dustin Walker <dwtimp@gmail.com>
2016-10-19 13:34:23 -0500

    some progress

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,13 +1,22 @@
 export default function createGameElement(data) {
-  const game = document.createElement('div');
-  game.classList.add('game-item');
-  game.innerHTML =
-  `<div className="frame">
-  <h2 class="game-item__name"</h2>
-  <h3 class="game-item__popularity</h3>
-  <h4 class="_id"</h4>
+  const item = document.createElement('li');
+  item.classList.add('game-item');
+  item.innerHTML =
+  `<div class="frame">
+  <h3 class="game-item__name"</h3>
+  <h4 class="game-item__popularity"</h4>
   </div>
   <img class="game-item__pic" src ="" alt=""></img>`;
-  const title = game.querySelector('.game-item__name');
-  title.innerText = game.name;
+
+  const title = item.querySelector('.game-item__name');
+  title.innerText = data.game;
+
+  const popularity = item.querySelector('.game-item__popularity');
+  popularity.innerText = data.popularity;
+
+  const pic = item.querySelector('.game-item__pic');
+  pic.src = data.box.large;
+  pic.alt = data.game;
+
+  return item;
 }
